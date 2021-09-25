0e194ed9b5a508c2b97b81564b5afa75dc263004
0xzhang <869566387@qq.com>
2021-09-26 05:27:23 +0800

    Up game of life.

---
+++ README.md
@@ -0,0 +1,8 @@
+# taichi-play
+
+使用太极图形做一些好玩的东西:stuck_out_tongue_winking_eye:
+
+## Game of Life
+
+![gol](game_of_life/gol.gif)
+
---
+++ game_of_life/game_of_life.py
@@ -0,0 +1,104 @@
+import taichi as ti
+
+ti.init(arch=ti.cpu)
+
+N = 128
+M = 3*N
+
+live_ratio = 0.6
+cells = ti.field(ti.f32, shape=(N, N))
+next_gen = ti.field(ti.f32, shape=(N, N))
+pixels = ti.field(ti.f32, shape=(M, M))
+
+
+@ti.kernel
+def init_random():
+    for i, j in cells:
+        val = ti.random()
+        if(val < live_ratio):
+            cells[i, j] = 1
+
+
+@ti.kernel
+def init_blinker():
+    step = 5
+    iters = N/step
+    for i in range(0, iters):
+        for j in range(0, iters):
+            cells[step*i + 1, step*j+2] = 1
+            cells[step*i + 2, step*j+2] = 1
+            cells[step*i + 3, step*j+2] = 1
+
+
+@ti.kernel
+def init_beacon():
+    step = 6
+    iters = N/step
+    for i in range(0, iters):
+        for j in range(0, iters):
+            cells[step*i + 1, step*j+3] = 1
+            cells[step*i + 2, step*j+4] = 1
+            cells[step*i + 1, step*j+4] = 1
+            cells[step*i + 3, step*j+1] = 1
+            cells[step*i + 4, step*j+1] = 1
+            cells[step*i + 4, step*j+2] = 1
+
+
+@ti.kernel
+def init_glider():
+    cells[0, N-2] = 1
+    cells[1, N-3] = 1
+    cells[2, N-1] = 1
+    cells[2, N-2] = 1
+    cells[2, N-3] = 1
+
+
+@ti.kernel
+def evolve():
+    neighbors = 0
+    for i, j in cells:
+        neighbors = int(cells[(i-1) % N, (j-1) % N]+cells[i, (j-1) % N] +
+                        cells[(i+1) % N, (j-1) % N] + cells[(i-1) % N, j] +
+                        cells[(i+1) % N, j] + cells[(i-1) % N, (j+1) % N] +
+                        cells[i, (j+1) % N]+cells[(i+1) % N, (j+1) % N])
+        if cells[i, j] == 1:
+            if (neighbors < 2) or (neighbors > 3):
+                next_gen[i, j] = 0
+        else:
+            if neighbors == 3:
+                next_gen[i, j] = 1
+
+
+@ti.kernel
+def zoom_in():
+    scale = M/N
+    for i, j in pixels:
+        pixels[i, j] = cells[int(i/scale), int(j/scale)]
+
+
+gui = ti.GUI("Game of Life", res=(M, M))
+
+init_random()
+# init_blinker()
+# init_beacon()
+# init_glider()
+next_gen = cells
+
+# while gui.running:
+#     evolve()
+#     cells = next_gen
+#     zoom_in()
+#     gui.set_image(pixels)
+#     gui.show()
+
+# Export to pngs and convert to mp4/gif.
+# ti video -f 50
+# ti gif -i video.mp4
+for i in range(300):
+    evolve()
+    cells = next_gen
+    zoom_in()
+    gui.set_image(pixels)
+    filename = f'frame_{i:05d}.png'
+    print(f'Frame {i} is recorded in {filename}')
+    gui.show(filename)
