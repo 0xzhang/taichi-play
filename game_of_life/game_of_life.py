a76d7be90f1ab15a73cc19b5e9f72491918f7975
0xzhang <aliceb0b@hotmail.com>
2021-10-14 23:02:35 +0800

    refactor: use ti.imresize() zoom in; decompose branches in evolve() to 2 for-loops and reduce twice field copy. (ref taichi examples: game_of_life)

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -1,121 +1,54 @@
 import taichi as ti
+import numpy as np
 
-ti.init(arch=ti.gpu)
+ti.init(arch=ti.cpu)
 
 N = 128
 # How many pixels do you want to represent one cell?
-M = 3 * N
+M = 5
 
 live_ratio = 0.6
 cells = ti.field(ti.i32, shape=(N, N))
-new_cells = ti.field(ti.i32, shape=(N, N))
-pixels = ti.field(ti.f32, shape=(M, M))
+neighbors = ti.field(ti.i32, shape=(N, N))
 
 
 @ti.kernel
-def init_random():
+def init():
     for I in ti.grouped(cells):
         val = ti.random()
         if (val < live_ratio):
             cells[I] = 1
 
 
-@ti.kernel
-def init_blinker():
-    step = 5
-    iters = N / step
-    for i in range(0, iters):
-        for j in range(0, iters):
-            cells[step * i + 1, step * j + 2] = 1
-            cells[step * i + 2, step * j + 2] = 1
-            cells[step * i + 3, step * j + 2] = 1
-
-
-@ti.kernel
-def init_beacon():
-    step = 6
-    iters = N / step
-    for i in range(0, iters):
-        for j in range(0, iters):
-            cells[step * i + 1, step * j + 3] = 1
-            cells[step * i + 2, step * j + 4] = 1
-            cells[step * i + 1, step * j + 4] = 1
-            cells[step * i + 3, step * j + 1] = 1
-            cells[step * i + 4, step * j + 1] = 1
-            cells[step * i + 4, step * j + 2] = 1
-
-
-@ti.kernel
-def init_glider():
-    assert N > 3
-    cells[0, N - 2] = 1
-    cells[1, N - 3] = 1
-    cells[2, N - 1] = 1
-    cells[2, N - 2] = 1
-    cells[2, N - 3] = 1
+@ti.func
+def count(i, j):
+    neighbors[i,
+              j] = (cells[(i - 1) % N, (j - 1) % N] + cells[i, (j - 1) % N] +
+                    cells[(i + 1) % N, (j - 1) % N] + cells[(i - 1) % N, j] +
+                    cells[(i + 1) % N, j] + cells[(i - 1) % N, (j + 1) % N] +
+                    cells[i, (j + 1) % N] + cells[(i + 1) % N, (j + 1) % N])
 
 
 @ti.kernel
 def evolve():
-    for I in ti.grouped(cells):
-        new_cells[I] = cells[I]
-
-    neighbors = 0
     for i, j in cells:
-        neighbors = (cells[(i - 1) % N, (j - 1) % N] + cells[i, (j - 1) % N] +
-                     cells[(i + 1) % N, (j - 1) % N] + cells[(i - 1) % N, j] +
-                     cells[(i + 1) % N, j] + cells[(i - 1) % N, (j + 1) % N] +
-                     cells[i, (j + 1) % N] + cells[(i + 1) % N, (j + 1) % N])
-        if cells[i, j] == 1:
-            if (neighbors < 2) or (neighbors > 3):
-                new_cells[i, j] = 0
+        count(i, j)
+
+    for I in ti.grouped(cells):
+        if neighbors[I] < 2 or neighbors[I] > 3:
+            cells[I] = 0
         else:
-            if neighbors == 3:
-                new_cells[i, j] = 1
-
-    for I in ti.grouped(cells):
-        cells[I] = new_cells[I]
-
-
-@ti.kernel
-def zoom_in():
-    # Limitations:
-    assert M >= N
-    scale = int(M / N)
-    # Increased computation, but more precise to deal each pixel.
-    for i, j in cells:
-        for x in range(scale):
-            for y in range(scale):
-                pixels[scale * i + x, scale * j + y] = cells[i, j]
-
-    # scale = N/M
-    # for i, j in pixels:
-    #     pixels[i, j] = cells[int(i*scale), int(j*scale)]
-
-
-def main():
-    gui = ti.GUI("Game of Life", res=(M, M))
-
-    init_random()
-    # init_blinker()
-    # init_beacon()
-    # init_glider()
-
-    while gui.running:
-        evolve()
-        zoom_in()
-        gui.set_image(pixels)
-        gui.show()
-
-    # Export to pngs and convert to mp4/gif.
-    # for i in range(300):
-    #     evolve()
-    #     zoom_in()
-    #     gui.set_image(pixels)
-    #     filename = f'frame_{i:05d}.png'
-    #     print(f'Frame {i} is recorded in {filename}')
-    #     gui.show(filename)
+            if neighbors[I] == 3:
+                cells[I] = 1
 
 
 if __name__ == '__main__':
-    main()
+    gui = ti.GUI("Game of Life", res=(M * N, M * N))
+    init()
+
+    while gui.running:
+        evolve()
+
+        # A handy method learned from `examples/simulation/game_of_life.py` for zoom in frame.
+        gui.set_image(ti.imresize(cells, M * N).astype(np.uint8) * 255)
+        gui.show()
---
+++ game_of_life/game_of_life_patterns.py
@@ -0,0 +1,106 @@
+import taichi as ti
+import numpy as np
+
+ti.init(arch=ti.gpu)
+
+N = 128
+# How many pixels do you want to represent one cell?
+M = 5
+
+live_ratio = 0.6
+cells = ti.field(ti.i32, shape=(N, N))
+neighbors = ti.field(ti.i32, shape=(N, N))
+
+
+@ti.kernel
+def init_random():
+    for I in ti.grouped(cells):
+        val = ti.random()
+        if (val < live_ratio):
+            cells[I] = 1
+
+
+@ti.kernel
+def init_glider():
+    assert N > 3
+    cells[0, N - 2] = 1
+    cells[1, N - 3] = 1
+    cells[2, N - 1] = 1
+    cells[2, N - 2] = 1
+    cells[2, N - 3] = 1
+
+
+@ti.kernel
+def init_blinker():
+    step = 5
+    iters = N / step
+    for i in range(0, iters):
+        for j in range(0, iters):
+            cells[step * i + 1, step * j + 2] = 1
+            cells[step * i + 2, step * j + 2] = 1
+            cells[step * i + 3, step * j + 2] = 1
+
+
+@ti.kernel
+def init_beacon():
+    step = 6
+    iters = N / step
+    for i in range(0, iters):
+        for j in range(0, iters):
+            cells[step * i + 1, step * j + 3] = 1
+            cells[step * i + 2, step * j + 4] = 1
+            cells[step * i + 1, step * j + 4] = 1
+            cells[step * i + 3, step * j + 1] = 1
+            cells[step * i + 4, step * j + 1] = 1
+            cells[step * i + 4, step * j + 2] = 1
+
+
+@ti.func
+def count(i, j):
+    neighbors[i,
+              j] = (cells[(i - 1) % N, (j - 1) % N] + cells[i, (j - 1) % N] +
+                    cells[(i + 1) % N, (j - 1) % N] + cells[(i - 1) % N, j] +
+                    cells[(i + 1) % N, j] + cells[(i - 1) % N, (j + 1) % N] +
+                    cells[i, (j + 1) % N] + cells[(i + 1) % N, (j + 1) % N])
+
+
+@ti.kernel
+def evolve():
+    for i, j in cells:
+        count(i, j)
+
+    for I in ti.grouped(cells):
+        if neighbors[I] < 2 or neighbors[I] > 3:
+            cells[I] = 0
+        else:
+            if neighbors[I] == 3:
+                cells[I] = 1
+
+
+def main():
+    gui = ti.GUI("Game of Life", res=(M * N, M * N))
+
+    init_random()
+    # init_glider()
+    # init_blinker()
+    # init_beacon()
+
+    while gui.running:
+        evolve()
+
+        # A handy method learned from `examples/simulation/game_of_life.py` for zoom in frame.
+        gui.set_image(ti.imresize(cells, M * N).astype(np.uint8) * 255)
+        gui.show()
+
+    # Export to pngs and convert to mp4/gif.
+    # for i in range(300):
+    #     evolve()
+    #     zoom_in()
+    #     gui.set_image(pixels)
+    #     filename = f'frame_{i:05d}.png'
+    #     print(f'Frame {i} is recorded in {filename}')
+    #     gui.show(filename)
+
+
+if __name__ == '__main__':
+    main()
